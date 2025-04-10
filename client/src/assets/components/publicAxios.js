9831a7a75f1b591075d9c718c08c1d0cf0619044
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-04-10 19:10:19 -0300

    json server with vercel added

--- client/src/assets/components/publicAxios.js
+++ client/src/assets/components/publicAxios.js
@@ -5,6 +5,9 @@ const publicAxios = axios.create({
   // baseURL: 'http://localhost:5000',
 
   // Production
-  baseURL: 'https://blushing-shimmer-latency.glitch.me/',
+  // baseURL: 'https://blushing-shimmer-latency.glitch.me/',
+
+  // Vercel
+  baseURL: "https://blog-server-five-ochre.vercel.app/",
 });
 export default publicAxios;
